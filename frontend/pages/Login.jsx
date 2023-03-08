a8a28480b7a68cb955459db31bf0f3e8453fe544
Jacky <jackypengsmail@gmail.com>
2023-03-08 11:03:37 -0500

    Changed login styling

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -5,6 +5,13 @@ import { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import { AuthContext } from '../contexts/authContext';
 
+
+
+
+//styling
+import "../CSS/Login.css"
+import { Col, Row, Container, Image, Form, Button } from "react-bootstrap"
+
 const Login = () => {
 
 
@@ -20,7 +27,7 @@ const Login = () => {
         if (hasToken) {
             // fake.logout();
             navigate(`/dashboard/${userId}`);
-        } 
+        }
     }, [hasToken]);
 
     async function submitLogin(e) {
@@ -54,24 +61,28 @@ const Login = () => {
         };
     }
     return (
-        <>
-            <div> Welcome back to PawPets! </div>
-            <form onSubmit={submitLogin}>
-                <div className="mb-3">
-                    <label htmlFor="inputUsername" className="form-label">Username</label>
-                    <input type="text" className="form-control" id="inputUsername" aria-describedby="username" onChange={(e) => setUsername(e.target.value)} value={username} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="inputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" onChange={(e) => setPassword(e.target.value)} value={password} />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-            <div>
-                <span>Dont have an account?</span>
-                <NavLink to="/signup">Sign up</NavLink>
-            </div>
-        </>
+        <Container>
+            <Row>
+                <Col> Welcome back to PawPets! </Col>
+            <Col>
+                <Form onSubmit={submitLogin}>
+                    <div className="mb-3">
+                        <label htmlFor="inputUsername" className="form-label">Username</label>
+                        <input type="text" className="form-control" id="inputUsername" aria-describedby="username" onChange={(e) => setUsername(e.target.value)} value={username} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="inputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" onChange={(e) => setPassword(e.target.value)} value={password} />
+                    </div>
+                    <Button type="submit" className="btn btn-primary">Login</Button>
+                </Form>
+            </Col>
+            <Col>
+                    <span>Dont have an account?</span>
+                    <NavLink to="/signup">Sign up</NavLink>
+            </Col>
+        </Row>
+        </Container >
     )
 }
 
