9e986176e06519f19e95882cd622e9deda7cb000
Jacky <jackypengsmail@gmail.com>
2023-03-30 21:35:08 -0400

    Updated styling

--- frontend/pages/Pets.jsx
+++ frontend/pages/Pets.jsx
@@ -14,7 +14,7 @@ const Pets = () => {
         <Row className="w-100 d-flex justify-content-center ">
           {pets.map((pet) => (
             <Col
-              className="pet-post pt-3 pb-3 pt-md-0 pb-md-0 gap-md-3 d-flex flex-column justify-content-center align-items-center"
+              className="pet-post pt-5 pb-5  gap-md-3 d-flex flex-column justify-content-center align-items-center"
               xs={10}
               sm={8}
               md={5}
@@ -26,7 +26,7 @@ const Pets = () => {
               <div className="img-container">
                 <Image className=" " src={`../public/${pet.link}`} alt="" />
               </div>
-              <div className="mt-md-3">Species: {pet.species}</div>
+              <div className="mt-3">Species: {pet.species}</div>
               <div>User: {pet.petUsername}</div>
             </Col>
           ))}
